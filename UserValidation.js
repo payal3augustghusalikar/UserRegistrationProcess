455ab39d5bc5744a44844fe243373523733c6e74
payalghusalikar <payal.ghusalikar9@gmail.com>
2020-12-23 03:00:13 +0530

    Added : validate last name and added switch cases

--- UserValidation.js
+++ UserValidation.js
@@ -1,12 +1,41 @@
 
-const print  = require('./UserValidationProcess');
+const validationProcess = require('./UserValidationProcess');
 const prompt = require('prompt-sync')();
 
 class UserValidation {
+
+    /**
+    * @description Takes user input and calls firstNameValidator function
+    */
     validateUserInputFirstName = () => {
         var firstName = prompt(`Enter your first name : `, firstName);
-        print.firstNameValidator(firstName);    
+        validationProcess.firstNameValidator(firstName);
+    }
+
+    validateUserInputLastName = () => {
+        var lastName = prompt(`Enter your last name : `, lastName);
+        validationProcess.lastNameValidator(lastName);
+    }
+
+    validation = () => {
+        console.log(`\n ****** welcome to user registration Process \n`);
+
+        while (true) {
+            var choice = prompt(`1. First Name  2. Last Name Enter your choice: `, choice);
+            switch (choice) {
+                case "1":
+                    object.validateUserInputFirstName();
+                    break;
+                case "2":
+                    object.validateUserInputLastName();
+                    break;
+                default:
+                    console.log("Please Enter right choice");
+                    break;
+            }
+        }
     }
 }
+
 const object = new UserValidation();
-object.validateUserInputFirstName();
+object.validation();
--- UserValidationProcess.js
+++ UserValidationProcess.js
@@ -1,12 +1,57 @@
-const FIRST_NAME_PATTERN = new RegExp("^[A-Z]{1}[a-z]{2,}$");
+/**
+ * @description constants variable is used to store regex pattern
+ * @constant {RegExp} NAME_PATTERN
+ */
+const NAME_PATTERN = new RegExp("^[A-Z]{1}[a-z]{2,}$");
 
+/**
+ * @description Class UserValidationProcess
+ */
 class UserValidationProcess {
+
+    /**
+     * @description checks the user input with regex pattern
+     * @param {string} firstName 
+     */
     firstNameValidator(firstName) {
-      
-        if (FIRST_NAME_PATTERN.test(firstName)) 
-            console.log("valid First Name");
-        else 
-            console.log("Invalid First Name");
+
+        if (NAME_PATTERN.test(firstName))
+            console.log("valid First Name \n");
+        else
+            console.log("Invalid First Name \n");
     }
+
+    /**
+     * @description checks the user input with regex pattern
+     * @param {string} lastName 
+     */
+    lastNameValidator(lastName) {
+        if (NAME_PATTERN.test(lastName))
+            console.log("valid Last Name \n");
+        else
+            console.log("Invalid Last Name \n");
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
 module.exports = new UserValidationProcess();
