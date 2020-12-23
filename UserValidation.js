59100453ad1156b36e83f5e70d83256188caec5f
payalghusalikar <payal.ghusalikar9@gmail.com>
2020-12-24 01:47:14 +0530

    Added : checked valid and invalid email samples

--- UserValidation.js
+++ UserValidation.js
@@ -1,5 +1,6 @@
 
 const validationProcess = require('./UserValidationProcess');
+const emailSamples = require('./EmailSamples.json');
 const prompt = require('prompt-sync')();
 
 class UserValidation {
@@ -12,46 +13,88 @@ class UserValidation {
         validationProcess.firstNameValidator(firstName);
     }
 
+    /**
+        * @description Takes user input and calls lastNameValidator function
+        */
     validateUserInputLastName = () => {
         var lastName = prompt(`Enter your last name : `, lastName);
         validationProcess.lastNameValidator(lastName);
     }
 
+
+    /**
+        * @description Takes user input and calls emailValidator function
+        */
     validateUserInputEmail = () => {
         var email = prompt(`Enter your Email : `, email);
         validationProcess.emailValidator(email);
     }
 
+    /**
+        * @description Takes user input and calls mobileNoValidator function
+        */
     validateUserInputMobileNo = () => {
         var mobileNo = prompt(`Enter your mobile number : `, mobileNo);
         validationProcess.mobileNoValidator(mobileNo);
     }
 
+    /**
+        * @description Takes user input and calls passwordValidator function
+        */
     validateUserInputPassword = () => {
         var password = prompt(`Enter your password : `, password);
         validationProcess.passwordValidator(password);
     }
 
+    /**
+        * @description fetch email from jsom file and calls validSamplesEmailValidator function
+        */
+    validateValidEmailSamples = () => {
+        console.log("-----Check for Valid Email Samples-----");
+
+        for (let i = 0; i < emailSamples.validEmailSamples.length; i++) {
+            let sampleEmail = emailSamples.validEmailSamples[i];
+            validationProcess.validSamplesEmailValidator(sampleEmail);
+        }
+    }
+
+    /**
+        * @description fetch email from jsom file and calls invalidSamplesEmailValidator function
+        */
+    validateInvalidEmailSamples = () => {
+        console.log("-----Check for invalid Email Samples-----");
+
+        for (let i = 0; i < emailSamples.invalidEmailSamples.length; i++) {
+            let sampleEmail = emailSamples.invalidEmailSamples[i];
+            validationProcess.invalidSamplesEmailValidator(sampleEmail);
+        }
+    }
+
     validation = () => {
         console.log(`\n ****** welcome to user registration Process ********\n`);
-
-        var choice = prompt(`1. First Name   2. Last Name   3. Email  4. Mobile No.  5. Password  Enter your choice: `, choice);
-        switch (choice) {
-            case "1":
+ 
+      var choice = prompt(`1. First Name   2. Last Name  3. Email  4. Mobile No.  5. Password  6. check valid email samples  7. check invalid email samples -> Enter your choice: `, choice);
+      var a = parseInt(choice) 
+        switch (a) {
+            case 1:
                 object.validateUserInputFirstName();
                 break;
-            case "2":
+            case 2:
                 object.validateUserInputLastName();
                 break;
-            case "3":
+            case 3:
                 object.validateUserInputEmail();
                 break;
-            case "4":
+            case 4:
                 object.validateUserInputMobileNo();
                 break;
-            case "5":
+            case 5:
                 object.validateUserInputPassword();
                 break;
+            case 6:
+                object.validateValidEmailSamples();
+            case 7:
+                object.validateInvalidEmailSamples();
             default:
                 console.log("Please Enter right choice");
                 break;
--- UserValidationProcess.js
+++ UserValidationProcess.js
@@ -3,7 +3,8 @@
  * @constant {RegExp} NAME_PATTERN
  */
 const NAME_PATTERN = new RegExp("^[A-Z]{1}[a-z]{2,}$");
-const EMAIL_PATTERN = new RegExp("^([a-z]{3,})([.]{0,1}[a-z]*)@([a-z]{2}).([a-z]{2})([.]{1}[a-z]{2}){0,1}$");
+//const EMAIL_PATTERN = new RegExp("^([a-z]{3,})([.]{0,1}[a-z]*)@([a-z]{2}).([a-z]{2})([.]{1}[a-z]{2}){0,1}$");
+const EMAIL_PATTERN = new RegExp(`^([A-Za-z\\d-_\\+]+)(\\.[A-Za-z\\d-_]+)?@([a-zA-Z\\d]+)\\.([a-zA-Z]{2,4})(\\.[A-Za-z]{2,4})?$`);
 const MOBILE_NO_PATTERN = new RegExp("^([0-9]{2})[ ]([1-9]{1}[0-9]{9})$");
 const PASSWORD_PATTERN = new RegExp("^((.*[A-Z]{1,})(.*[0-9]{1,})(.*[\\~\\!\\@\\#\\$\\%\\^\\&\\*])(.*[A-Za-z0-9]{5,}))$");
 
@@ -35,6 +36,10 @@ class UserValidationProcess {
             console.log("Invalid Last Name \n");
     }
 
+    /**
+         * @description checks the user input with regex pattern
+         * @param {string} email
+         */
     emailValidator(email) {
         if (EMAIL_PATTERN.test(email))
             console.log("valid email \n");
@@ -42,6 +47,10 @@ class UserValidationProcess {
             console.log("Invalid email \n");
     }
 
+    /**
+         * @description checks the user input with regex pattern
+         * @param {Number} mobileNo 
+         */
     mobileNoValidator(mobileNo) {
         if (MOBILE_NO_PATTERN.test(mobileNo))
             console.log("valid mobile No \n");
@@ -49,11 +58,36 @@ class UserValidationProcess {
             console.log("Invalid mobile No \n");
     }
 
+    /**
+         * @description checks the user input with regex pattern
+         * @param {string} password
+         */
     passwordValidator(password) {
         if (PASSWORD_PATTERN.test(password))
             console.log("valid Password  \n");
         else
             console.log("Invalid Password  \n");
     }
+
+
+    /**
+         * @description checks email with emailregex pattern
+         * @param {string} sampleEmail
+         */
+    validSamplesEmailValidator = (sampleEmail) => {
+        if (EMAIL_PATTERN.test(sampleEmail))
+            console.log(`sample valid Email is passed`);
+        else
+            console.log(`sample valid Email is not passed`);
+    }
+
+    /**
+         * @description checks email with emailregex pattern
+         * @param {string} sampleEmail
+         */
+    invalidSamplesEmailValidator = (sampleEmail) => {
+        return (EMAIL_PATTERN.test(sampleEmail)) ? `sample invalid Email is not passed` : `sample invalid Email is passed`
+    }
 }
+
 module.exports = new UserValidationProcess();
